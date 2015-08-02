7508233d92115e3408af0757a5f74bf493ce87f5
Ivan <office@emcode.net>
2015-08-02 23:50:55 +0200

    Working

--- bigmac.py
+++ bigmac.py
@@ -5,7 +5,7 @@ import sys
 
 net_adapter = sys.argv[1]
 
-change = 0;
+change = False;
 
 if(len(sys.argv) >= 3):
     change = sys.argv[2]
@@ -19,15 +19,21 @@ def new_mac():
         ]
     return ':'.join(map(lambda x: "%02x" % x, mac))
 
+new_mac = new_mac()
+
 cat_args = "/sys/class/net/" + net_adapter + "/address"
 
 old_mac = subprocess.Popen(["cat", cat_args], stdout=subprocess.PIPE)
 (curr_mac, err) = old_mac.communicate()
 
+if(change == False):
+    print "Current MAC for " + net_adapter + " is: " + curr_mac
 
 if(change and change == "new"):
     # execute code for mac change with subprocess
-
-if(!change):
-    print curr_mac
-
+    print 'Changing MAC from ' + curr_mac + "to " + new_mac + " for adapter " + net_adapter + "..."
+    subprocess.call(["ifconfig", net_adapter, "down"])
+    subprocess.call(["ifconfig", net_adapter, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", net_adapter, "up"])
+    
+    
